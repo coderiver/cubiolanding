1acd7d825c595f57927bacfaf98c3d81f0efcf7c
akella <akella.a@gmail.com>
2014-10-29 17:29:48 +0100

    popups for landing

--- js/common.js
+++ js/common.js
@@ -9,17 +9,35 @@ $(document).ready(function() {
 	});
 
 
-	$('.scr1 .scr__wrappics').sticky({topSpacing:50});
+	if($('.scr1').length>0){
+		$('.scr1 .scr__wrappics').sticky({topSpacing:50});
 
-	toggler =  $(".scr2").offset().top;
+		toggler =  $(".scr2").offset().top;
 
-	$(window).scroll(function(event) {
-		if($(window).scrollTop()>toggler - 400){
-			$('.scr__wrappics').addClass('is-showed2');
-		}
-		else{
-			$('.scr__wrappics').removeClass('is-showed2');
-		}
+		$(window).scroll(function(event) {
+			if($(window).scrollTop()>toggler - 400){
+				$('.scr__wrappics').addClass('is-showed2');
+			}
+			else{
+				$('.scr__wrappics').removeClass('is-showed2');
+			}
+		});
+	}
+
+
+
+	//popups
+	$('.js-popup').click(function(event) {
+		$('.popup').removeClass('is-shown');
+		pop = $(this).data('popup');
+		$('#'+pop).addClass('is-shown')
+		$('.overlay').fadeIn();
+		return false;
+	});
+
+	$('.popup__close,.overlay').click(function(event) {
+		$('.popup').removeClass('is-shown')
+		$('.overlay').fadeOut();
 	});
 
 
